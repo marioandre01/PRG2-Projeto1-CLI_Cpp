400236a1667252de773099dd78baf4561db169cf
Mario Andre <mario_andre.l.a@hotmail.com>
2020-06-10 15:25:34 -0300

    Projeto finalizado

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=projeto1-cli
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/projeto1-cli
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=projeto1-cli.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/projeto1-cli.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=projeto1-cli
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/projeto1-cli
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=projeto1-cli.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/projeto1-cli.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
